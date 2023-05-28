59eb4dd7513ced990189de726309807c7a416178
Devani Zelvia <125328215+Evanii13@users.noreply.github.com>
2023-05-28 16:14:20 +0800

    Add files via upload

---
+++ linked list/linked list - Copy.h
@@ -0,0 +1,39 @@
+class Node{
+	
+	public:
+		int value; 
+		Node *next; 
+		
+		Node(){ 
+			next = NULL;
+	
+			
+			
+		}
+		
+};
+class LinkedList{
+	
+
+	
+	
+	public: 
+		Node *head;
+		Node *tail;
+	void insertToHead(int);
+	void insertToTail(int);
+	void insertAfter(int, int); 
+	
+	
+	void deleteToHead();
+	void deleteToTail();
+	void deleteByValue(int);
+	
+	
+	void printAll();
+	LinkedList() {
+		
+		head = tail =NULL;
+		
+	}
+};
